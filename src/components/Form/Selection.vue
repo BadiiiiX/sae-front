fadfbb7eaa63b7a2de0e5b9b60c2751b5a829c0f
BadiiiX <contact@mehdi-ali.me>
2023-06-22 11:45:29 +0200

    a

--- src/components/Form/Selection.vue
+++ src/components/Form/Selection.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
 
-import {onMounted, reactive, ref} from "vue";
+import {onMounted, reactive, ref, watchEffect} from "vue";
 import axios from "axios";
 import {computedAsync, useAsyncState} from "@vueuse/core";
 
 const apiUrl = "http://localhost:3000";
+
+const selectedCategory = ref(null);
+const selectedSubCategory = ref(null);
+const selectedSubSubCategory = ref(null);
+const haveSubElements = ref(false);
+const selectedAliments = ref([]);
 
 const {state: categories, isReady: categoriesIsReady, isLoading: categoriesIsLoading} = useAsyncState(
     axios
@@ -14,45 +20,76 @@ const {state: categories, isReady: categoriesIsReady, isLoading: categoriesIsLoa
     {immediate: true},
 )
 
-const {state: aliments, isReady: alimentsIsReady, isLoading: alimentsIsLoading} = useAsyncState(
+const {state: subCategories, isReady: subCategoriesIsReady, isLoading: subCategoriesIsLoading} = useAsyncState(
     axios
-        .get(`${apiUrl}/aliment/all`)
+        .get(`${apiUrl}/subcategory/all`)
         .then(t => t.data),
     [],
     {immediate: true},
 )
 
-const chosenCategories = ref([]);
+const {state: subSubCategories, isReady: subSubCategoriesIsReady, isLoading: subSubCategoriesIsLoading} = useAsyncState(
+    axios
+        .get(`${apiUrl}/subsubcategory/all`)
+        .then(t => t.data),
+    [],
+    {immediate: true},
+)
 
-const alimentsToShow = ref([]);
+const getCategories = computedAsync(() => categories.value.map((t: any) => ({label: t.name, value: t.code})));
 
-const getCategories = () => {
-  return categories.value.map(t => {
-    return {
-      label: t.name,
-      value: t.code
-    }
-  })
-}
+const getSubCategories = computedAsync(() => subCategories.value
+    .filter((t: any) => {
+      if (Array.isArray(selectedCategory.value)) {
+        return (selectedCategory.value as any[]).includes(t.code.slice(0, 2))
+      }
+
+      return []
+    })
+    .map((t: any) => ({label: t.name, value: t.code})));
+
+const getSubSubCategories = (() => subSubCategories.value
+    .filter((t: any) => {
+      if (Array.isArray(selectedSubCategory.value)) {
+        return (selectedSubCategory.value as any[]).includes(t.code.slice(0, 4))
+      }
+
+      return []
+    })
+    .map((t: any) => ({label: t.name, value: t.code})));
 
 const getAliments = () => {
-  if(chosenCategories.value.length === 0) {
-    return [];
-  }
-  const res = aliments.value
-      .filter(t => { return chosenCategories.value.includes(t.category.code) || chosenCategories.value.includes(t.subCategory.code) || chosenCategories.value.includes(t.subSubCategory.code) })
-      .map(t => {
-        return {
-          label: t.name,
-          value: t.code
-        }
-      })
-  console.log(chosenCategories.value);
-  console.log(res);
+  const getLastCode = (selectedSubSubCategory.value === null || selectedSubSubCategory.value.length <= 0)
+      ? selectedSubCategory.value
+      : selectedSubSubCategory.value;
 
-  return res
+  const result = [];
+
+  for(const code of getLastCode) {
+    const {state, isReady, isLoading} = useAsyncState(
+        axios
+            .get(`${apiUrl}/aliment/search/${code}`)
+            .then(t => t.data),
+        []
+    )
+
+    result.push(...state.value)
+  }
+
+  return result;
 }
 
+const resetIfEmpty = (v: string[], _node: any) => {
+  if (v === null || v.length === 0) {
+    selectedSubCategory.value = null;
+    selectedSubSubCategory.value = null;
+  }
+}
+
+watchEffect(() => {
+  selectedSubCategory.value;
+  haveSubElements.value = getSubSubCategories().length > 0;
+})
 </script>
 
 <template>
@@ -61,9 +98,6 @@ const getAliments = () => {
     <h3 class="justify">Afin de remplir le formulaire, veuillez choisir vos dix aliments préférés.</h3>
   </header>
 
-  <pre>{{alimentsToShow}}</pre>
-
-
   <FormKit
       type="form"
       #default="{ value }"
@@ -71,28 +105,49 @@ const getAliments = () => {
   >
     <FormKit
         v-if="categoriesIsReady"
+        v-model="selectedCategory"
         type="taglist"
         name="categoryFilter"
         label="Filtrez par catégorie"
         :options="getCategories"
-        v-model="chosenCategories"
-        :close-on-select="false"
-        @input="alimentsToShow = getAliments()"
+        @input="resetIfEmpty"
     />
 
     <FormKit
-        name="aliments"
-        type="transferlist"
-        help="Veuillez filtrer par la catégorie que vous souhaitez avant de sélectionner vos aliments."
-        source-label="Aliments"
-        target-label="Vos Aliments"
-        source-empty-message="Aucun aliment, chargement en cours..."
-        target-empty-message="Choisissez vos aliments favoris"
-        searchable
-        :options="alimentsToShow"
-        max="10"
-        placeholder="Sélectionnez vos aliments"
+        v-if="subCategoriesIsReady && selectedCategory !== null && selectedCategory.length > 0"
+        type="taglist"
+        name="subCategoryFilter"
+        label="Filtrez par sous-catégorie"
+        :options="getSubCategories"
+        v-model="selectedSubCategory"
+        @input="resetIfEmpty"
     />
+
+    <FormKit
+        v-if="subSubCategoriesIsReady && selectedSubCategory !== null && selectedSubCategory.length > 0 && haveSubElements"
+        type="taglist"
+        v-model="selectedSubSubCategory"
+        name="subSubCategoryFilter"
+        label="Filtrez par sous-sous-catégorie"
+        :options="getSubSubCategories"
+
+    />
+
+    <FormKit
+        v-if="selectedSubCategory !== null && selectedSubCategory.length > 0 && (!haveSubElements || selectedSubSubCategory !== null && selectedSubSubCategory.length > 0)"
+        type="transferlist"
+        name="aliments"
+        v-model="selectedAliments"
+        label="Choisissez vos aliments"
+        :options="getAliments"
+        max="10"
+    />
+
+    <pre>
+      {{ selectedCategory }}
+      {{ selectedSubCategory }}
+      {{ selectedSubSubCategory }}
+    </pre>
 
   </FormKit>
 </template>
